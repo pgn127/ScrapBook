032b56c484eacd3530fdfdd18efee9c8b8cefa9d
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-08-30 17:13:40 +1000

    Initial Commit

---
+++ Scrapbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scrapbook
+//
+//  Created by Pamela Needle on 8/30/15.
+//  Copyright (c) 2015 Pamela Needle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
